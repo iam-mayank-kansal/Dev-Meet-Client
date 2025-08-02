ba0f01e296bfedd870dfdd2c53ab1487ea323f91
Mayank Kansal <165586266+iam-mayank-kansal@users.noreply.github.com>
2025-08-02 15:47:43 +0530

    changes

--- dev-meet-client/src/app/layout.tsx
+++ dev-meet-client/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "../store/Provider"; 
 import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+// import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +36,7 @@ export default function RootLayout({
           <main className="flex-grow ">
             {children}
           </main>
-          <Footer />
+          {/* <Footer /> */}
         </ReduxProvider>
       </body>
     </html>
