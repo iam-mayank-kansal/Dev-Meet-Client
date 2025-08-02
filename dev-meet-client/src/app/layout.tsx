7017546dce39c52dc3ed530dae85858af323ed5f
Mayank Kansal <165586266+iam-mayank-kansal@users.noreply.github.com>
2025-08-02 15:37:21 +0530

    changes

--- dev-meet-client/src/app/layout.tsx
+++ dev-meet-client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Footer } from "@/components/Footer";
-import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer/index";
+import { Navbar } from "@/components/Navbar/index";
 import { ReduxProvider } from "../store/Provider"; 
 
 const geistSans = Geist({
