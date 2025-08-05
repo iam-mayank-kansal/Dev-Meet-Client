bf6a707892c16c9d618bdd7523470f22e7ef758c
Mayank Kansal <165586266+iam-mayank-kansal@users.noreply.github.com>
2025-08-06 00:10:40 +0530

    comented meeting code

--- dev-meet-client/src/app/layout.tsx
+++ dev-meet-client/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "../store/Provider"; 
 import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+import { Footer } from "../components/Footer";
 
 
 const geistSans = Geist({
